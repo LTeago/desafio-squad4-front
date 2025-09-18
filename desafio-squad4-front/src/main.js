2e239fa57d0dd3a185cd2cbf1052ea448236f0df
Levy <levyfernandes14@gmail.com>
2025-09-18 02:29:32 -0300

    Criando backdrop do modal

--- desafio-squad4-front/src/main.js
+++ desafio-squad4-front/src/main.js
@@ -4,6 +4,7 @@ import "./style/grid.css";
 import "./style/style.css";
 import "./style/section.css";
 import "./style/menu.css";
+import "./style/modal.css";
 import "./style/display.css";
 import "./style/servicos.css";
 import "./style/footer.css";
@@ -12,14 +13,18 @@ const dropdown = document.getElementById('dropdown-container');
 const dropdownToggleBtn = document.getElementById('dropdown-toggle-btn');
 const dropdownCloseBtn = document.getElementById('dropdown-close-btn');
 
+const modalBackdrop = document.getElementById('modal-backdrop');
+
 function toggleElement(element) {
     element.classList.toggle('on');
 }
 
 dropdownToggleBtn.addEventListener('click', () => {
     toggleElement(dropdown);
+    toggleElement(modalBackdrop);
 });
 
 dropdownCloseBtn.addEventListener('click', () => {
     toggleElement(dropdown);
+    toggleElement(modalBackdrop); 
 });
